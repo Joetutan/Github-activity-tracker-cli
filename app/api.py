b5a55d9933a661bca6d20d138ae6900a6d2b9756
joetutan <noobianjoe@outlook.com>
2025-12-17 11:22:51 +0200

    add events to json function

--- app/api.py
+++ app/api.py
@@ -3,19 +3,23 @@ import requests
 
 def get_events(user_name: str) -> dict:
     
-    '''Fetch recent activity of the specified GitHub user '''
-    url = f"https://api.github.com/users/{user_name}/events"
+    # Fetch recent activity of the parsed username GitHub account 
+    url = f"https://api.github.com/users/{user_name}/events/public"
 
+    # Header requesting github api json format specifying version
     t_headers = {
         "Accept" : "application/vnd.github+json",
         "X-GitHub-Api-Version": "2022-11-28"
     }
-
+    # Assign response 
     response = requests.get(url, headers=t_headers)
-    print("Status Code:", response.status_code)
+
+    #print("Status Code:", response.status_code)
 
     if response.ok:
         data = response.json()
         return data
     else:
         return  response.text
+
+
