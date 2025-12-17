be27cef0bf186b99bdd99e5fdb5c7b310686589c
joetutan <40818345+Joetutang@users.noreply.github.com>
2025-12-17 11:22:51 +0200

    add events to json function

--- app/cli.py
+++ app/cli.py
@@ -1,22 +1,48 @@
 import argparse
 from app.api import get_events
-#import json
+import json
 from pathlib import Path
+from datetime import datetime
 
-file_path = Path("./app/data.json")
+file_path = Path("./data.json")
+
+if file_path.exists() :
+    with open(file_path, "r") as f:
+        activity = json.load(f)
+else:
+    activity = {}
+
 
 def main():
     parser = argparse.ArgumentParser(description="Github-User-Activity - fetch user activity and display it in the terminal")
     parser.add_argument("username", help="Provide GitHub username as an argument")
     args = parser.parse_args()
 
-    user_data = get_events(args.username)
-    print(f"{user_data}")
+    events = get_events(args.username)
+    
+    if events:
+        
+        for event in events:
+            event_type = event.get("type")
+            event_date = event.get("created_at")
+            
+            if not event_type or not event_date:
+                continue
 
-    '''
-    if "error" in user_data:
-        print(f"error: {user_data}")
+            date = datetime.fromisoformat(event_date.replace("Z", "")).date()
+            
+            if str(date) not in activity :
+                activity[str(date)] = {}
+            
+            if event_type not in activity[str(date)]:
+                activity[str(date)][event_type] = 1
+            else:
+                activity[str(date)][event_type] += 1
+
+            with open(file_path, "w") as f:
+                json.dump(activity, f, indent=4)
     else:
-        with open(file_path, "w") as f:
-                json.dump(user_data, f, indent=4)
-'''
+        print(f"Invalid_username: {args.username}")
+  
+ 
+        
